3becaac6ba867381a19045ffffa958a7e4a7eab7
Stephen Moore <judo2000@comcast.net>
2022-01-29 11:42:34 -0700

    Create userController functions to create and fetch all users

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
+const {
+    createUser,
+    getAllUsers
+} = require('../../../controllers/userController');
 
-
-
+// /api/users
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser)
+    
 module.exports = router
